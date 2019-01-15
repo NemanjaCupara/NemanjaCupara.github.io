dbbc8995b687f4c959b9aba370545ef5befa3c0b
NemanjaCupara <41583384+NemanjaCupara@users.noreply.github.com>
2019-01-15 18:28:35 +0100

    NewWeb

--- js/jsText.js
+++ js/jsText.js
@@ -40,6 +40,7 @@ TxtRotate.prototype.tick = function() {
 };
 
 window.onload = function() {
+	    document.getElementById("my_audio").play();
   var elements = document.getElementsByClassName('txt-rotate');
   for (var i=0; i<elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-rotate');
